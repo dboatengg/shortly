5379c7a95c149da3064eff89e3505959f12fa2b5
dboatengg <dicksonboateng67@gmail.com>
2022-08-12 10:42:18 +0000

    modify functionality of navMenu

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav__menu");
+const navLink = document.querySelectorAll(".nav__link");
+mainBody = document.getElementsByTagName("main")[0];
+heroBody = document.querySelector(".hero");
 
-hamburger.addEventListener("click", mobileMenu);
+hamburger.addEventListener("click", openMenu);
 
-function mobileMenu() {
+function openMenu() {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
 }
-
-const navLink = document.querySelectorAll(".nav__link");
 
 navLink.forEach((n) => n.addEventListener("click", closeMenu));
 
@@ -16,3 +17,7 @@ function closeMenu() {
   hamburger.classList.remove("active");
   navMenu.classList.remove("active");
 }
+
+mainBody.addEventListener("click", closeMenu);
+heroBody.addEventListener("click", closeMenu);
+console.log(mainBody);
